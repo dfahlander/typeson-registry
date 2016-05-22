8e3a4c93e9a242a2da0495938f25e9445cb652fa
David Fahlander <david.fahlander@gmail.com>
2016-05-22 02:16:23 +0200

    Made typedarray be backed by base64 rather than special unicode char handling. Reason is that base64 is more efficient on UTF-8 even though the special unicode format would be more efficient in utf-16, but who uses utf-16 today? No, stick to standards and use something expected.

--- types/typedarray.js
+++ types/typedarray.js
@@ -1,3 +1,5 @@
+var b64 = require ('base64-arraybuffer');
+
 function isTypedArray(Class) {
     return Class && Class.BYTES_PER_ELEMENT && /\Array ?\(\) ?{\s+\[native code\]/.test(Class);        
 }
@@ -18,35 +20,17 @@ module.exports = {
         function (x) { return isTypedArray(x.constructor); },
         // Encapsulator
         function (a) {
-            // Convert Typed Array to direct unicode string, but record invalid chars separately since they
-            // would otherwise get lost when string is encoded to an UTF-x format.
-            var uint16View = new Uint16Array(a.buffer, a.byteOffset, Math.floor(a.byteLength / 2));
-            var invalids = uint16View.filter(isSpecial).map(function (ch) { return ch - 0xd7E0; });
             return {
-                data: String.fromCharCode.apply(null, uint16View),
-                inv: String.fromCharCode.apply(null, invalids),
-                type: funcName(a.constructor), // "Uint8Array", "Int8Array", "Uint16Array", ...
-                xtra: a.byteLength % 2 ? a[a.byteLength-1] : undefined
+                data: b64.encode (a.buffer, a.byteOffset, a.byteLength),
+                type: funcName(a.constructor) // "Uint8Array", "Int8Array", "Uint16Array", ...
             };
         },
         // Reviver
         function (o) {
-            var data = o.data,
-                invalids = o.inv,
-                extraByte = 'xtra' in o,
-                l = data.length, i = invalids.length, ch,
-                buf = new ArrayBuffer(2*l + extraByte),
-                arr = new Uint16Array(buf);
-            while (l--) {
-                if (isSpecial(arr[l] = data.charCodeAt(l))) {
-                    arr[l] = invalids.charCodeAt(--i) + 0xd7e0;
-                }
-            }
+            var buf = b64.decode (o.data);
             var TypedArray = _global[o.type]; // Int8Array, Uint8Array, Int16Array, Uint16Array, ...
             if (!isTypedArray(TypedArray)) throw new Error ("Could not instanciate " + o.type);
-            var rv = new TypedArray(buf);
-            if (extraByte) arr[buf.length - 1] = o.xtra; // Only possible for 8-bit arrays.
-            return rv;
+            return new TypedArray(buf);
         }
     ]
 };
