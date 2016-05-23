c8490ee899420c57b1d8ce442d3ed353c34a4c04
David Fahlander <david.fahlander@gmail.com>
2016-05-24 00:55:38 +0200

    Added ImageData type (browser-specific)

---
+++ types/imagedata.js
@@ -0,0 +1,9 @@
+/** ImageData is browser / DOM specific.
+ * Requires arraybuffer.js in order for the returned Uint8ClampedArray to be encapsulated as well,
+ * or if use with socket.io, socket.io will take care of that.
+*/
+exports.ImageData = [
+    function (x) { return x instanceof ImageData; },
+    function (d) { return {array: d.array, width: d.width, height: d.height}; },
+    function (o) { return new ImageData(o.array, o.width, o.height); }
+];
