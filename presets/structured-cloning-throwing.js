8cf8cfaae4431c38a138be98df900a86066bb2ef
Brett Zamir <brettz9@yahoo.com>
2017-02-10 17:59:15 +0800

    - Prohibit `Object.prototype` as it is an exotic object and therefore    per SCA ought to be rejected (though not forbidden currently in Chrome)

--- presets/structured-cloning-throwing.js
+++ presets/structured-cloning-throwing.js
@@ -7,14 +7,15 @@ module.exports = require('./structured-cloning').concat({checkDataCloneException
     var stringTag = ({}.toString.call(val).slice(8, -1));
     if (typeof val === 'symbol' || // Symbol's `toStringTag` is only "Symbol" for its initial value, so we check `typeof`
         [
-            'Function', // All functions
-            'Arguments', // An exotic object
+            'Function', // All functions including bound function exotic objects
+            'Arguments', // A non-array exotic object
             'Error', // `Error` and other errors have the [[ErrorData]] internal slot and give "Error"
             'Proxy', // Proper `toStringTag` not yet implemented in Chrome/Firefox/Node
             'Promise', // Promise instances have an extra slot ([[PromiseState]]) but not throwing in Chrome `postMessage`
             'WeakMap', // WeakMap instances have an extra slot ([[WeakSetData]]) but not throwing in Chrome `postMessage`
             'WeakSet' // WeakSet instances have an extra slot ([[WeakSetData]]) but not throwing in Chrome `postMessage`
-        ].includes(stringTag)
+        ].includes(stringTag) ||
+        val === Object.prototype // A non-array exotic object but not throwing in Chrome `postMessage`
     ) {
         throw new DOMException('The object cannot be cloned.', 'DataCloneError');
     }
