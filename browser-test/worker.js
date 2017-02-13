dbf93397022859144fe6cbc36d9d080df3a02ca2
Brett Zamir <brettz9@yahoo.com>
2017-02-13 21:28:33 +0800

    - Testing (workers): Elaborate more on logging

--- browser-test/worker.js
+++ browser-test/worker.js
@@ -5,6 +5,7 @@ var TSON = new Typeson().register(Typeson.presets.postMessage);
 
 onmessage = function (ev) {
     console.log("Worker got: " + TSON.stringify(TSON.revive(ev.data)));
+    console.log("Worker revival of error is an `Error`: " + (TSON.revive(ev.data).d instanceof Error));
     postMessage(ev.data);
 };
 
