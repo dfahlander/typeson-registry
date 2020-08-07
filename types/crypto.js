8efec9d9246cd71cabae1a3b13e29787c9f5323b
Septs <github@septs.pw>
2020-08-07 21:10:02 +0800

    add crypto key support

---
+++ types/crypto.js
@@ -0,0 +1,21 @@
+import Typeson from "typeson";
+
+const crypto = {
+    cryptokey: {
+        test(x) {
+            return Typeson.toStringTag(x) === "CryptoKey";
+        },
+        replaceAsync(key) {
+            return crypto.subtle.exportKey("jwk", key).then(jwk => ({
+                jwk,
+                algorithm: key.algorithm,
+                usages: key.usages
+            }));
+        },
+        reviveAsync({ jwk, algorithm, usages }) {
+            return crypto.subtle.importKey("jwk", jwk, algorithm, true, usages);
+        }
+    }
+};
+
+export default crypto;
