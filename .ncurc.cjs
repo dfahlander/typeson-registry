3a542616e528aa4276849392cdd2af44f6da670f
Brett Zamir <brettz9@yahoo.com>
2023-05-04 16:14:41 -0700

    chore: update devDeps.

---
+++ .ncurc.cjs
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    reject: [
+        // Todo: Try again after some time as caused number of tests to fail,
+        //        whether in Node 18 or 20
+        'jsdom'
+    ]
+};
